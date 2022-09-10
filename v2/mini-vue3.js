0175f4fac2023c3a12ff867cd060abd6ab1c6c95
unknown <416212316@qq.com>
2022-09-10 10:33:22 +0800

    fix

--- v2/mini-vue3.js
+++ v2/mini-vue3.js
@@ -141,7 +141,7 @@ function renderComponentRoot(
     // 返回上一个正在渲染的实例对象
     const prev = setCurrentRenderingInstance(instance)
     result = render.call(proxy)
-    // 再设置当前的渲染对象上一个，具体场景是嵌套循环渲染的时候，渲染完子组件，再去渲染父组件
+    // 再设置当前的渲染对象上一个，具体场景是嵌套循环渲染的时候，渲染完子组件，再返回去渲染父组件
     setCurrentRenderingInstance(prev)
     return result
 }
