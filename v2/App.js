93bd5d6788f05f42271c4c04cefa72a3a3534a47
unknown <416212316@qq.com>
2022-09-10 17:43:12 +0800

    fix

--- v2/App.js
+++ v2/App.js
@@ -5,6 +5,7 @@ import { Component } from './Component.js'
 export const App = {
     name: 'App',
     components: {
+        // 注册局部组件
         Component
     },
     setup() {
@@ -15,7 +16,9 @@ export const App = {
     },
     // 一般在 SFC 的模式组件下我们是不用写 render 选项的，render 选项是由 template 进行编译生成
     render() {
+        // 获取局部组件
         const comp = resolveComponent('Component')
+        // 获取全局组件
         const RouterView = resolveComponent('RouterView')
         return createVNode('div', {}, [
             createVNode('p', {}, 'Hi Vue3 Component param count is：' + this.count),
