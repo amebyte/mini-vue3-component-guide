1e5ef4876a04d695a90d431bc6439e36280c6b1c
unknown <416212316@qq.com>
2022-09-11 00:02:22 +0800

    element-plus

--- v3/App.js
+++ v3/App.js
@@ -1,13 +1,8 @@
 import { ref } from '../node_modules/@vue/reactivity/dist/reactivity.esm-browser.js'
 import { resolveComponent } from './resolveAssets.js'
 import { createVNode } from './vnode.js'
-import { Component } from './Component.js'
 export const App = {
     name: 'App',
-    components: {
-        // 注册局部组件
-        Component
-    },
     setup() {
        const count = ref(520) 
        return {
@@ -16,14 +11,14 @@ export const App = {
     },
     // 一般在 SFC 的模式组件下我们是不用写 render 选项的，render 选项是由 template 进行编译生成
     render() {
-        // 获取局部组件
-        const comp = resolveComponent('Component')
-        // 获取全局组件
-        const RouterView = resolveComponent('RouterView')
+        // 获取组件库 Button 组件
+        const Button = resolveComponent('Button')
+        // 获取组件库 Icon 组件
+        const Icon = resolveComponent('Icon')
         return createVNode('div', {}, [
             createVNode('p', {}, 'Hi Vue3 Component param count is：' + this.count),
-            createVNode(comp),
-            createVNode(RouterView)
+            createVNode(Button),
+            createVNode(Icon)
         ])
     }
 }
--- v3/Button.js
+++ v3/Button.js
@@ -1,15 +1,15 @@
 import { ref } from '../node_modules/@vue/reactivity/dist/reactivity.esm-browser.js'
 import { createVNode } from './vnode.js'
-export const Component = {
-    name: 'Component',
+export const Button = {
+    name: 'Button',
     setup() {
-       const txt = ref('Component') 
+       const txt = ref('组件库 Button 组件') 
        return {
         txt
        }
     },
     // 一般在 SFC 的模式组件下我们是不用写 render 选项的，render 选项是由 template 进行编译生成
     render() {
-        return createVNode('div', {}, 'Hi Vue3 Component param txt is：' + this.txt)
+        return createVNode('div', {}, 'Button param txt is：' + this.txt)
     }
 }
